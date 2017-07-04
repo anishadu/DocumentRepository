441a2c1390d86d86e94a4d62c5614c62a900cea0
anishpanthi <anish.panthi@gmail.com>
2017-07-04 17:02:37 -0500

    Project Setup

---
+++ src/com/app/docrepo/main/DocumentRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.docrepo.main;
+
+/**
+ *
+ * @author Anish Panthi
+ */
+public class DocumentRepository {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
